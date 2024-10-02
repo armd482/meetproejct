5c4357e5799b61ee433f125e629cca69cbcc6c2b
armd482 <armd482@naver.com>
2024-10-02 21:24:37 +0900

    Style: 시간 null일 경우 빈 컴포넌트 렌더링

--- src/app/[code]/component/MeetInfoBar.tsx
+++ src/app/[code]/component/MeetInfoBar.tsx
@@ -8,6 +8,10 @@ export default function MeetInfoBar() {
   const code = usePathname().slice(1);
   const date = useCurrentDate();
 
+  if (!date) {
+    return <div />;
+  }
+
   return (
     <div className='flex flex-1 items-center gap-3 truncate sm-md:hidden'>
       <p>{formatTime(date)}</p>
--- src/app/landing/component/part/Header/CurrentDate.tsx
+++ src/app/landing/component/part/Header/CurrentDate.tsx
@@ -5,6 +5,9 @@ import { useCurrentDate } from '@/hook';
 
 export default function CurrentDate() {
   const time = useCurrentDate();
+  if (!time) {
+    return <div />;
+  }
   return (
     <div className='max-[472px]:hidden flex items-center gap-2 p-3 text-lg font-medium text-gray-500'>
       <p>{formatTime(time)}</p>
